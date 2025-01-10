ffc07fc02974e777df41cbbcea9829f33c20690c
Sarok Ádám <sarok.adam@gmail.com>
2025-01-10 23:42:12 +0100

    feat: 2020 day 1

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ if (!dayToSolve) {
   console.error('No day specified run with npm run dev {day}');
   process.exit(1);
 }
-console.log(`Solving Day #${args[0]}`);
+console.log('\x1b[34m%s\x1b[0m', `Solving Day #${args[0]}`);
 (async () => {
   let input = '';
   const puzzleName = args[0];
@@ -20,7 +20,10 @@ console.log(`Solving Day #${args[0]}`);
     process.exit(1);
   }
   const { first, second }: Puzzle = await import(`./days/${puzzleName}/Puzzle`);
-
+  let time = performance.now();
   console.log(first(input));
+  console.log('\x1b[32m%s\x1b[0m', `Part1 completed in: ${(performance.now() - time).toFixed(2)} ms`);
+  time = performance.now();
   console.log(second(input));
+  console.log('\x1b[32m%s\x1b[0m', `Part2 completed in: ${(performance.now() - time).toFixed(2)} ms`);
 })();
