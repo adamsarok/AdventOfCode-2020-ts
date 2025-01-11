762f165f93f12f9c5f2c087cb727c1d59fa5aa68
Sarok Ádám <sarok.adam@gmail.com>
2025-01-11 20:49:14 +0100

    feat: 2020 day 4

--- src/days/3/Puzzle.ts
+++ src/days/3/Puzzle.ts
@@ -1,35 +1,33 @@
 const checkSlope = (map: string[], xSlope: number, ySlope: number): number => {
   let trees = 0;
   let x = 0, y = 0;
+
   while (y < map.length) {
-    if (x >= map[y].length - 1) {
-      x -= map[y].length - 1;
-    } // this is incorrect, check
+    if (x >= map[y].length) {
+      x -= map[y].length;
+    }
     if (map[y][x] == '#') {
       trees++;
     }
-    // console.log(`x${x}:y${y}->${map[y][x]}`);
     x += xSlope; y += ySlope;
   }
-  console.log(`x${xSlope}:y${ySlope}->${trees}`);
   return trees;
 };
 
 const first = (input: string) => {
-  const map = input.split('\n');
+  const map = input.split('\n').map(x => x.trim());
   return checkSlope(map, 3, 1);
 };
 
 const expectedFirstSolution = 7;
 
 const second = (input: string) => {
-  const map = input.split('\n');
-  // return checkSlope(map, 1, 1);
-  return checkSlope(map, 1, 1) // bad
-    * checkSlope(map, 3, 1)  // ok
-    * checkSlope(map, 5, 1)  // ok
-    * checkSlope(map, 7, 1)  // ok
-    * checkSlope(map, 1, 2); // ok
+  const map = input.split('\n').map(x => x.trim());
+  return checkSlope(map, 1, 1)
+    * checkSlope(map, 3, 1)
+    * checkSlope(map, 5, 1)
+    * checkSlope(map, 7, 1)
+    * checkSlope(map, 1, 2);
 };
 
 const expectedSecondSolution = 336;
