c605e6683bd38909255696f5752eaa0d6bfb2765
Ádám Sarok <sarok.adam@gmail.com>
2025-01-16 22:28:31 +0100

    feat: 2020 day 9

--- src/days/9/Puzzle.ts
+++ src/days/9/Puzzle.ts
@@ -1,13 +1,56 @@
 const first = (input: string) => {
-  return 'solution 1';
+  const numbers: number[] = [];
+  for (const l of input.split('\n').map((x) => x.trim())) {
+    numbers.push(parseInt(l));
+  }
+  const windowLen = numbers.length < 100 ? 5 : 25;
+  for (let t = windowLen; t < numbers.length; t++) {
+    if (!checkNum(t, numbers, windowLen)) {
+      return numbers[t];
+    }
+  }
+  return -1;
 };
 
-const expectedFirstSolution = 'solution 1';
+const expectedFirstSolution = 127;
 
 const second = (input: string) => {
-  return 'solution 2';
+  const numbers: number[] = [];
+  for (const l of input.split('\n').map((x) => x.trim())) {
+    numbers.push(parseInt(l));
+  }
+  const toFind = first(input);
+  let windowStart = 0,
+    windowEnd = 0;
+  let window = numbers[windowStart];
+  while (windowEnd < numbers.length) {
+    if (window === toFind) {
+      const w = numbers.slice(windowStart, windowEnd);
+      return Math.min(...w) + Math.max(...w);
+    } else if (window < toFind) {
+      windowEnd++;
+      window += numbers[windowEnd];
+    } else {
+      window -= numbers[windowStart];
+      windowStart++;
+    }
+  }
+  return -1;
 };
 
-const expectedSecondSolution = 'solution 2';
+const expectedSecondSolution = 62;
 
 export { first, expectedFirstSolution, second, expectedSecondSolution };
+
+function checkNum(t: number, numbers: number[], windowLen: number) {
+  const target = numbers[t];
+  for (let a = t - windowLen; a < t - 1; a++) {
+    const aNum = numbers[a];
+    for (let b = t - windowLen + 1; b < t; b++) {
+      if (target === aNum + numbers[b]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
