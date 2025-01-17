fdb27fc989b63e3f1fadf4485b9e4c123e0ccfc1
Ádám Sarok <sarok.adam@gmail.com>
2025-01-17 19:59:57 +0100

    feat: 2020 day 10

--- src/days/10/Puzzle.ts
+++ src/days/10/Puzzle.ts
@@ -1,13 +1,50 @@
 const first = (input: string) => {
-  return 'solution 1';
+  // isn't this just a simple sort???
+  const numbers = parseInput(input);
+  let ones = 0,
+    threes = 0;
+  for (let i = 1; i < numbers.length; i++) {
+    switch (numbers[i] - numbers[i - 1]) {
+      case 1:
+        ones++;
+        break;
+      case 3:
+        threes++;
+        break;
+    }
+  }
+  return ones * threes;
 };
 
-const expectedFirstSolution = 'solution 1';
+const expectedFirstSolution = 220;
 
 const second = (input: string) => {
-  return 'solution 2';
+  const numbers = parseInput(input);
+  const waysToGetToIndex: number[] = [];
+  waysToGetToIndex.push(1);
+  for (let i = 1; i < numbers.length; i++) {
+    let prev = i - 1;
+    let result = 0;
+    while (prev >= 0 && numbers[i] - numbers[prev] <= 3) {
+      result += waysToGetToIndex[prev];
+      prev--;
+    }
+    waysToGetToIndex[i] = result;
+  }
+  return waysToGetToIndex[waysToGetToIndex.length - 1];
 };
 
-const expectedSecondSolution = 'solution 2';
+const expectedSecondSolution = 8;
 
 export { first, expectedFirstSolution, second, expectedSecondSolution };
+
+function parseInput(input: string) {
+  const numbers: number[] = [];
+  for (const l of input.split('\n').map((x) => x.trim())) {
+    numbers.push(parseInt(l));
+  }
+  numbers.push(0);
+  numbers.sort((a, b) => a - b);
+  numbers.push(numbers[numbers.length - 1] + 3);
+  return numbers;
+}
