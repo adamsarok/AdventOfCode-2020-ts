b4aae45ba226e1e5c9aaf24c3695ad33b66c7a46
Ádám Sarok <sarok.adam@gmail.com>
2025-01-24 23:57:57 +0100

    feat: day11 incomplete

---
+++ src/days/11/Puzzle.ts
@@ -0,0 +1,39 @@
+// import { type Vec, getNeighbourVecs } from '../../types/Vec';
+
+// const first = (input: string) => {
+//   const seats = input.split('\n').map((x) => x.trim().split(''));
+//   let iterations = 0;
+//   let state = -1;
+//   while (!iterate(seats, -1)) {
+//     iterations++;
+//   }
+//   // is it enough to count num of seats to determine stable state?
+//   return iterations;
+// };
+
+// const expectedFirstSolution = 37;
+
+// const second = (input: string) => {
+//   return 'solution 2';
+// };
+
+// const expectedSecondSolution = 'solution 2';
+
+// export { first, expectedFirstSolution, second, expectedSecondSolution };
+
+// function iterate(seats: string[][]) {
+//   for (let x = 0; x < seats.length; x++) {
+//     for (let y = 0; y < seats[x].length; y++) {}
+//   }
+// }
+
+// const neighbors = getNeighbourVecs();
+
+// function getNeighbours(x: number, y: number) {
+//   const result: string[] = [];
+//   for (let n of neighbors) {
+//     const xx: x + n.x;
+//     const yy: y + n.y;
+//     if (xx >= 0 && xx < )
+//   }
+// }
--- src/types/Vec.ts
+++ src/types/Vec.ts
@@ -1,6 +1,17 @@
-type Vec = {
+export type Vec = {
   x: number;
   y: number;
 };
 
-export default Vec;
+export function getNeighbourVecs(): Vec[] {
+  return [
+    { x: -1, y: -1 },
+    { x: 0, y: -1 },
+    { x: 1, y: -1 },
+    { x: -1, y: 0 },
+    { x: 1, y: 0 },
+    { x: -1, y: 1 },
+    { x: 0, y: 1 },
+    { x: 1, y: 1 },
+  ];
+}
