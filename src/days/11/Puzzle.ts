89afae1bb23ac445c66f9228dfed0394f7f933c5
Sarok Ádám <sarok.adam@gmail.com>
2025-01-26 20:35:14 +0100

    feat: 2020 day 11 incomplete

--- src/days/11/Puzzle.ts
+++ src/days/11/Puzzle.ts
@@ -1,39 +1,109 @@
-// import { type Vec, getNeighbourVecs } from '../../types/Vec';
+import { type Vec, getNeighbourVecs } from '../../types/Vec';
+let width = 0,
+  height = 0;
 
-// const first = (input: string) => {
-//   const seats = input.split('\n').map((x) => x.trim().split(''));
-//   let iterations = 0;
-//   let state = -1;
-//   while (!iterate(seats, -1)) {
-//     iterations++;
-//   }
-//   // is it enough to count num of seats to determine stable state?
-//   return iterations;
-// };
+let seats: string[][] = [];
 
-// const expectedFirstSolution = 37;
+const first = (input: string) => {
+  seats = input.split('\n').map((x) => x.trim().split(''));
+  height = seats.length;
+  width = seats[0].length;
+  let iterations = 0;
+  let lastState: number[] = [];
+  while (iterations <= 1) {
+    const r = iterate(seats);
+    console.log(`iter:${iterations} ->`);
+    for (const row of seats) {
+      console.log(row);
+    }
+    iterations++;
+    if (arraysEqual(lastState, r)) {
+      return iterations;
+    }
+    lastState = r;
+  }
+  // I dont think its enough to count occupied seat cnt
+  return -1;
+};
 
-// const second = (input: string) => {
-//   return 'solution 2';
-// };
+const expectedFirstSolution = 37;
 
-// const expectedSecondSolution = 'solution 2';
+const second = (input: string) => {
+  return 'solution 2';
+};
 
-// export { first, expectedFirstSolution, second, expectedSecondSolution };
+const expectedSecondSolution = 'solution 2';
 
-// function iterate(seats: string[][]) {
-//   for (let x = 0; x < seats.length; x++) {
-//     for (let y = 0; y < seats[x].length; y++) {}
-//   }
-// }
+export { first, expectedFirstSolution, second, expectedSecondSolution };
 
-// const neighbors = getNeighbourVecs();
+function arraysEqual(arr1: number[], arr2: number[]): boolean {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
 
-// function getNeighbours(x: number, y: number) {
-//   const result: string[] = [];
-//   for (let n of neighbors) {
-//     const xx: x + n.x;
-//     const yy: y + n.y;
-//     if (xx >= 0 && xx < )
-//   }
-// }
+function iterate() {
+  const result: string[][] = [];
+  const occupiedIndices: number[] = [];
+  let index = 0;
+  for (let x = 0; x < width; x++) {
+    const row: string[] = [];
+    for (let y = 0; y < height; y++) {
+      switch (seats[y][x]) {
+        case '.':
+          row.push('.');
+          break;
+        case 'L':
+          if (getNeighbourOccupiedCnt(x, y) === 0) {
+            row.push('#');
+            occupiedIndices.push(index);
+          } else {
+            row.push('L');
+          }
+          break;
+        case '#':
+          if (getNeighbourOccupiedCnt(x, y) >= 4) {
+            row.push('L');
+          } else {
+            row.push('#');
+            occupiedIndices.push(index);
+          }
+          break;
+      }
+      index++;
+    }
+    result.push(row);
+  }
+  // console.log(result);
+  seats = result;
+  return occupiedIndices;
+}
+
+const neighbors = getNeighbourVecs();
+
+function getNeighbourOccupiedCnt(x: number, y: number) {
+  let occupied = 0;
+  for (const n of neighbors) {
+    const xx = x + n.x;
+    const yy = y + n.y;
+    if (
+      xx >= 0 &&
+      xx < width &&
+      yy >= 0 &&
+      yy < height &&
+      seats[y][x] === '#'
+    ) {
+      if (x === 5 && y === 0) {
+        console.log(`${xx}:${yy}`);
+      }
+      occupied++;
+    }
+  }
+  return occupied;
+}
